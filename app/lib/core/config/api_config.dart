b94fa5966f05b7a24a0982f699f5f5510c54e28a
douge <douge@dougedeMacBook-Pro.local>
2026-09-26 13:18:22 +0800

    feat: api地址更新

--- app/lib/core/config/api_config.dart
+++ app/lib/core/config/api_config.dart
@@ -5,8 +5,8 @@ class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
     // defaultValue: 'https://conflux.wobufang.com'
-    defaultValue: 'http://127.0.0.1:3001',
+    // defaultValue: 'http://127.0.0.1:3001',
     // defaultValue:'http://192.168.10.4:3001'
-    // defaultValue:'http://47.97.67.47:3002'
+    defaultValue:'http://47.97.67.47:3002'
   );
 }
